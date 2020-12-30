dcb435c644d2c4a9fbac53c3ac5b4bdb2d6fd91f
ParvinEyvazov <pervin.eyvazov1999@gmail.com>
2020-12-30 14:43:25 +0300

    realtime spica devkit bucket updated

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,7 +6,6 @@ import {
   ViewChild,
 } from '@angular/core';
 import * as Bucket from '@spica-devkit/bucket';
-import { error } from 'protractor';
 
 @Component({
   selector: 'app-root',
@@ -41,23 +40,33 @@ export class AppComponent {
   initiateCardElement() {
     console.log('aa');
     Bucket.initialize({
-      apikey: '57xb19kj1rzlat',
-      publicUrl: 'https://master.spicaengine.com/api',
+      apikey: 'ajjbym18ki4asz2m',
+      publicUrl: 'https://test-4061d.hq.spicaengine.com/api',
     });
 
-    //console.log(Bucket);
-    Bucket.data.getAll('5fe1c20c19f862002c7b0697').then(data => console.log(data))
-    Bucket.data.realtime.getAll("5fe1c20c19f862002c7b0697").subscribe(
-      (data) => {
-        console.log('data: ', data);
-      },
-      (error) => {
-        console.log('err: ', error);
-      },
-      () => {
-        console.log('completed: ');
+    /*
+     {
+        filter: JSON.stringify({ status: 'good' }),
       }
-    );
+    */
+    //console.log(Bucket);
+    // Bucket.data.realtime
+    //   .getAll('5fec45a4dfdd0f002c91f4c5')
+    //   .toPromise()
+    //   .then((data) => console.log(data));
+
+    Bucket.data.realtime
+      .getAll('5fec45a4dfdd0f002c91f4c5', {
+        filter: JSON.stringify({ status: 'good' }),
+      })
+      .subscribe(
+        (data) => {
+          console.log('data: ', data);
+        },
+        (error) => {
+          console.log('err: ', error);
+        }
+      );
 
     // Giving a base style here, but most of the style is in scss file
     const cardStyle = {
